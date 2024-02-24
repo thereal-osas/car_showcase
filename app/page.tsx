99ffa34b45ffbb3995dc17f97f56f191915413a1
Osas Abraham <abrahamosas3@gmail.com>
2024-02-24 10:25:51 +0000

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
-import Image from "next/image";
+import { CustomFilter, Hero, SearchBar } from "@/components";
 
 export default function Home() {
-  return <main className="overflow-hidden"></main>;
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          {/* <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
+          </div> */}
+        </div>
+      </div>
+    </main>
+  );
 }
